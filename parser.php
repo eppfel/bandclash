96629a7177510b5b9a90d12c30821ecfb15fa3fb
Felix Epp <study@felixepp.de>
2012-06-26 17:59:06 +0200

    parser class skeleton

---
+++ parser.php
@@ -0,0 +1,21 @@
+<?php
+require_once('dbhelper.php');
+
+/**
+* Parser to extract Data from a plain HTML site
+*/
+class BCParser extends DBHelper
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+
+	public function getChartsByArtist ($artistName)
+	{
+		//parse a Site
+		return $triples;
+	}
+}
+?>
